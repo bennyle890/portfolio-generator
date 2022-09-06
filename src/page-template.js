1c6c6d4f0847b7f6942b321a7aea63baabb39d06
Benny <benny.le890@gmail.com>
2022-09-06 02:03:46 -0400

    2nd thing done

--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,5 @@
-
+module.exports = (name, github) => {
+    return `
     <!DOCTYPE html>
     <html lang="en">
     
@@ -10,8 +11,10 @@
     </head>
     
     <body>
-      <h1>jane</h1>
-      <h2><a href="https://github.com/janehub">Github</a></h2>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
     </body>
     </html>
-    
+    `;
+  };
+  
